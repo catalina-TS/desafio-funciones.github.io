6609bd607d367776a7aa70236c0899efd96ce79e
catalina_TS <catatolvett@gmail.com>
2025-04-20 12:40:10 -0400

    Desafío ejercicio 1

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,11 @@
+/* Transforma esta declaración de función a una función de expresión */
+
+function example(a, b, c){
+    return a+b+c
+}
+
+/*Conversión a función de expresión*/ 
+
+const example = (a, b, c) => {
+    return a+b+c
+}
